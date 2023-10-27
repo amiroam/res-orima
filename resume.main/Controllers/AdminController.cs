0a794adf70c224de3b6d72974ed19829e6436263
Amiroorima <amirsalimi1394@gmail.com>
2023-10-27 12:17:50 +0330

    step 2

---
+++ resume.main/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿#region using
+using Microsoft.AspNetCore.Mvc;
+namespace resume.main.Controllers
+#endregion
+
+
+{
+	public class AdminController : Controller
+	{
+		#region AdminDashboard
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#endregion
+
+	}
+}
